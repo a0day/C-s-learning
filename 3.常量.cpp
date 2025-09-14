2baf41cd8c5c04694605f428415db35970c9dcf5
a0day <huangsheng0789@sjtu.edu.cn>
2025-09-14 16:02:03 +0800

    Add files via upload

--- 3.常量.cpp
+++ 3.常量.cpp
@@ -8,16 +8,16 @@ using namespace std;
 #define Day 7
 
 
-int main() {
-	
-	cout << "一周有" << Day  <<  "天 " << endl ;
-	
-	const int month = 12;
-
-	cout << "一年有" << month << "个月份 " << endl;
-
-	
-	system("pause");
-
-	return 0;
-}
+//int main() {
+//	
+//	cout << "一周有" << Day  <<  "天 " << endl ;
+//	
+//	const int month = 12;
+//
+//	cout << "一年有" << month << "个月份 " << endl;
+//
+//	
+//	system("pause");
+//
+//	return 0;
+//}
